1d35f7d8b95dd212c9e3c7bf29d5801ed88466a6
East <29425738+eastmaels@users.noreply.github.com>
2025-09-15 13:53:01 +0900

    add console logs for allowed origin

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const app = express();
 
 // CORS Configuration
 const allowedOrigins = process.env.ALLOWED_ORIGINS ? process.env.ALLOWED_ORIGINS.split(',') : [];
+console.log('allowed origins', allowedOrigins)
 
 if (process.env.NODE_ENV === 'production') {
     console.log('Allowed Origins:', allowedOrigins);
